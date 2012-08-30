db48e747b5ff18e11a2659393fdbcccc756d185c
Chris Markle <markle976@gmail.com>
2012-08-30 13:34:13 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var connect = require('connect'),
+	http = require('http'),
+	io = require('socket.io');
+
+var app = connect()
+  .use(connect.favicon())
+  .use(connect.logger('dev'))
+  .use(connect.static('public'))
+  .use(connect.directory('public'))
+  .use(function(req, res){
+    res.end('Hello from Connect!\n');
+  });
+
+var server = http.createServer(app);
+
+/*server.use('/',
+    connect.router(function(app) {
+        app.get('', handleGet);
+        //app.post('', handlePost);
+    })
+);*/
+
+// Listen for Socket.IO events
+var ioApp = io.listen(server);
+
+ioApp
+	.of('/addText')
+	.on('connection', function(socket) {
+		socket.on('add text', function(data) {
+			console.log(data.user + ": " + data.text);
+      //sahkdf aksdh akds
+			socket.broadcast.emit('msg received', data.user, data.text);
+			socket.emit('msg received', data.user, data.text);
+		});
+	});
+
+
+
+
+server.listen(3000, "192.168.43.109");
