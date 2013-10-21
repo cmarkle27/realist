bba9de6b6306979d057d1647a6cf5269ad772184
Chris Markle <cmarkle@turner.com>
2013-10-21 08:24:43 -0400

    update angular to use socket.io, and clean up

--- public/js/realistApp.js
+++ public/js/realistApp.js
@@ -1,26 +1,70 @@
 // 'use strict';
 
-var Realist = angular.module('realistApp', ['ui.bootstrap']);
-  // .config(['$routeProvider', function($routeProvider) {
+var RealistApp = angular.module('realistApp', [
+  'ui.bootstrap',
+  'realistApp.services',
+  'realistApp.controllers'
+]);
+
+  // Realist.config(['$routeProvider', function($routeProvider) {
   //   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   //   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   //   $routeProvider.otherwise({redirectTo: '/view1'});
   // }]);
 
-Realist.controller('ListController',['$scope', function($scope) {
-  
-  $scope.items = [
-    {text:'feed1', done:true},
-    {text:'feed2', done:true},
-    {text:'other feed', done:true},
-    {text:'more feed', done:true},
-    {text:'another feed', done:false}
-  ];
+/* Controllers */
+(function() {
+  var RealistControllers = angular.module('realistApp.controllers', []);
+  RealistControllers.controller('ListController', function($scope, socket) {
 
-  $scope.addItem = function() {
-    $scope.items.push({text:$scope.todoText, done:false});
-    $scope.todoText = '';
-    console.log($scope.items);
-  };  
+    socket.on('send:name', function (data) {
+      $scope.name = data.name;
+    });
 
-}]);
+    $scope.items = [
+      {text:'feed1', done:true},
+      {text:'feed2', done:true},
+      {text:'other feed', done:true},
+      {text:'more feed', done:true},
+      {text:'another feed', done:false}
+    ];
+
+    $scope.addItem = function() {
+      $scope.items.push({text:$scope.todoText, done:false});
+      $scope.todoText = '';
+      console.log($scope.items);
+    }; 
+    
+  });
+
+})();
+
+
+/* Services */
+(function() {
+  var RealistServices = angular.module('realistApp.services', []);
+  RealistServices.factory('socket', function($rootScope) {
+    var socket = io.connect();
+    return {
+      on: function(eventName, callback) {
+        socket.on(eventName, function() {  
+          var args = arguments;
+          $rootScope.$apply(function() {
+            callback.apply(socket, args);
+          });
+        });
+      },
+      emit: function(eventName, data, callback) {
+        socket.emit(eventName, data, function() {
+          var args = arguments;
+          $rootScope.$apply(function() {
+            if (callback) {
+              callback.apply(socket, args);
+            }
+          });
+        })
+      }
+    };
+  });  
+
+})();
